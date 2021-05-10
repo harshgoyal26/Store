5f405c55dc06af7b32b49899bf1aaab6d6c620c4
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2021-05-10 11:31:37 +0530

    Routing of Pages using React Router Dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import './bootstrap.min.css'
-import App from './App'
-
-// NOTHING IMPORTANT //
-// import reportWebVitals from './reportWebVitals'
-// 
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import "./bootstrap.min.css"
+import App from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// reportWebVitals();
+  document.getElementById("root")
+)
