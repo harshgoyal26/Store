cc9d70c6eedcbfa5131337a113fa8605afaa3926
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2021-05-13 21:38:46 +0530

    Cart Action and Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,24 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers"
+import { cartReducer } from "./reducers/cartReducers"
 
 const reducers = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
-const initialState = {}
+// This initial state can be used to hold authentication token fron local storage
+// Or cart items from local storage that are already added
+
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : []
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromLocalStorage,
+  },
+}
 const middleware = [thunk]
 
 const store = createStore(
