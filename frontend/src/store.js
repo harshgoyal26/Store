d6e660213867e1e443a9272affeacc6bdbed2fce
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2021-05-16 11:06:56 +0530

    User Login Functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,12 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
-
+import { userLoginReducer } from "./reducers/userReducers"
 const reducers = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 // This initial state can be used to hold authentication token fron local storage
 // Or cart items from local storage that are already added
@@ -18,9 +19,17 @@ const reducers = combineReducers({
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : []
+
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromLocalStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromLocalStorage,
   },
 }
 const middleware = [thunk]
