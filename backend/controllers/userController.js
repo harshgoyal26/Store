db7f6088f6f1f5954792ff0201e8f3b0cc0b74ae
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2021-05-16 13:32:03 +0530

    User profile PUT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
   })
   if (user) {
-    res.json({
+    res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -50,7 +50,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @Desc - GET USER PROFILE
+// @Desc - (GET) USER PROFILE
 // @Extended Route - /profile PRIVATE
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
@@ -67,4 +67,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @Desc - Update User Profile (PUT)
+// @Extended Route - /profile PRIVATE
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+    const updatedUser = await user.save()
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id, updatedUser.name),
+    })
+  } else {
+    res.status(404)
+    throw new Error("User Not Found")
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
