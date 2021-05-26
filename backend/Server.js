b84e94bf4bda07b39667ba323285b3ad421fdd56
Nishthavan dahiya <nishthavandahiya7@gmail.com>
2021-05-26 16:04:22 +0530

    Multer upload

--- backend/Server.js
+++ backend/Server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import ConnectDB from "./DatabaseConfig/dbconfig.js"
@@ -5,6 +6,7 @@ import colors from "colors"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 const app = express()
@@ -17,10 +19,15 @@ ConnectDB()
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENTID)
 )
+
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
 // 404 Status error
 app.use(notFound)
 // Customized Error Handler
