e079b131b0ff9c3de62565046def1d2fec8d0134
Harsh <goyal.harsh739@gmail.com>
2022-01-11 19:24:14 +0530

    modified footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Renewed Product Store</Col>
+          <Col className='text-center py-3'>Copyright &copy; Renewed Store</Col>
         </Row>
       </Container>
     </footer>
