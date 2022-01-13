6809ace4fa07e89e7f7a2e3b1fc1801c0373f50c
Harsh <goyal.harsh739@gmail.com>
2022-01-13 17:32:13 +0530

    final commit

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -99,7 +99,7 @@ const UsersListScreen = ({ history, match }) => {
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
-                  <td>$ {product.price}</td>
+                  <td>₹ {product.price}</td>
                   <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
